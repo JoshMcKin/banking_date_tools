8d8a8cb558360c9d2621409781dd4b85573da4a0
Joshua Mckinney <joshmckin@gmail.com>
2015-12-02 09:47:03 -0600

    remove to_date

--- lib/banking_date_tools/extend_ruby.rb
+++ lib/banking_date_tools/extend_ruby.rb
@@ -4,16 +4,3 @@ Time.class_eval do
     ((self.to_f) *1000).to_i
   end
 end
-
-String.class_eval do
-  US_DATE_REGEX = /^[0-9]{1,2}[\/\-][0-9]{1,2}[\/\-][0-9]{4}/.freeze
-  
-  def to_date
-    if self =~ US_DATE_REGEX
-      Date.strptime(self.gsub('/','-'), "%m-%d-%Y")
-    else
-      Date.parse(self)
-    end
-  end
-
-end
